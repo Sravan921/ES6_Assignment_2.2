52061acb97d2f87d01b611314e6e4d5d7d8dae19
Sravan921 <sukhavasi92@gmail.com>
2016-12-27 18:28:08 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as calc from 'calculation';
 
-console.log(calc.add(5, 4));
-console.log(calc.subtract(5, 4));
-console.log(calc.multiply(5, 4));
-console.log(calc.square(5));
+console.log(calc.add(6, 5));
+console.log(calc.subtract(6, 5));
+console.log(calc.multiply(6, 5));
+console.log(calc.square(6));
